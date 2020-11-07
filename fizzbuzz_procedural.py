ae0134044ad511eca3c92fabcc8fc58999291d93
D3v <deviljin112@users.noreply.github.com>
2020-11-07 14:18:22 +0000

    Update fizzbuzz_procedural.py

--- fizzbuzz_procedural.py
+++ fizzbuzz_procedural.py
@@ -1,23 +1,42 @@
-number_list = [k for k in range(101) if k != 0]
-
-
 def main():
-    fizz = input("What should be the divider number for 'Fizz'?\n=> ")
-    buzz = input("What should be the divider number for 'Buzz'?\n=> ")
+    number_list = [i for i in range(101) if i != 0]
+    divisible_nums = {}
 
-    if fizz.isdigit() and buzz.isdigit():
-        fizz, buzz = int(fizz), int(buzz)
-        for i in range(len(number_list)):
-            if number_list[i] % fizz == 0 and number_list[i] % buzz == 0:
-                number_list[i] = "FizzBuzz"
-            elif number_list[i] % fizz == 0:
-                number_list[i] = "Fizz"
-            elif number_list[i] % buzz == 0:
-                number_list[i] = "Buzz"
-        print(number_list)
+    print("How many numbers would you like?")
+    count = input("=> ")
+
+    if count.isdigit():
+        count = int(count)
+        i = 0
+
+        while i < count:
+            print("Choose a number and word.\nExample: 3 Fizz.")
+            try:
+                num, word = input("=> ").split(" ")
+            except:
+                print("Invalid format!\nFormat: NUM WORD.")
+            else:
+                if num.isdigit():
+                    i += 1
+                    divisible_nums[int(num)] = word
+                else:
+                    print("This is not a valid number!\nTry again.")
+
+        output = [str(i) for i in range(101) if i != 0]
+        for x in divisible_nums.keys():
+            for y in number_list:
+                if y % x == 0:
+                    if output[y - 1].isdigit():
+                        output[y - 1] = divisible_nums[x]
+                    else:
+                        output[y - 1] += divisible_nums[x]
+
+        print(output)
+
     else:
-        print("You need to use integer numbers!")
+        print("That is not a valid integer number")
 
 
 if __name__ == "__main__":
+    print("-=FizzBuzz=-")
     main()
